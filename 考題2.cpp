dcde1250cda386ef566b0b5770d0229835749688
Gordon665 <60079372+Gordon665@users.noreply.github.com>
2020-01-20 08:01:39 +0800

    Initial commit

---
+++ 考題2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "mysql++.h"
+using namespace std;
+
+int main()
+{
+    const char* db = "database1";
+    const char* server = "127.0.0.1";
+    const char* user = "Gordon";
+    const char* password = "cbn77534";
+    mysqlpp::Connection conn(false);
+    if (conn.connect(db, server, user, password))
+    {
+        cout << "connect succeed." << endl;
+        mysqlpp::Query query = conn.query("SELECT * FROM database1.memberdata");
+        if (mysqlpp::StoreQueryResult res = query.store())
+        {
+            cout.setf(ios::left);
+            cout << setw(20) << "ID" <<
+                    setw(20) << "Account" <<
+                    setw(20) << "Password" <<
+                    setw(20) << "Name" <<
+                    setw(20) << "Phone" <<
+                    endl;
+            mysqlpp::StoreQueryResult::const_iterator it;
+            for (it = res.begin(); it != res.end(); ++it)
+            {
+                mysqlpp::Row row = *it;
+                cout << setw(20) << row[0] << ' ' <<
+                        setw(20) << row[1] << ' ' <<
+                        setw(20) << row[2] << ' ' <<
+                        setw(20) << row[3] << ' ' <<
+                        setw(20) << row[4] << ' ' <<
+                        endl;
+            }
+        }
+    }
+    else
+    {
+        cout << "connect fail." << endl;
+    }
+    return 0;
+}
